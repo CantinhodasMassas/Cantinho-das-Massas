a0e08ce4f399c7d84d507f3eece46a997fe2a49b
CantinhodasMassas <simiaomartinsgisleide@gmail.com>
2026-06-01 14:35:32 -0300

    Atualização do cardapio 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 let carrinho = [];
+
+function adicionarAoCarrinho(item) {
+    carrinho.push(item);
+
+    alert(
+        item + "\n\n" +
+        "Itens no carrinho: " + carrinho.length
+    );
+
+    console.log(carrinho);
+}
