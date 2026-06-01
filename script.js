21c1060c306d9f9fb4cc2f540a7dc0891847d044
CantinhodasMassas <simiaomartinsgisleide@gmail.com>
2026-06-01 15:20:53 -0300

    Atualização do cardapio 2

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let carrinho = [];
 
-function adicionarAoCarrinho(item) {
-    carrinho.push(item);
+function adicionarAoCarrinho(item, preco) {
+
+    carrinho = [{
+        nome: item,
+        preco: preco
+    }];
 
     atualizarCarrinho();
 }
@@ -10,12 +14,18 @@ function atualizarCarrinho() {
 
     let lista = document.getElementById("lista-carrinho");
 
+    let total = 0;
+
     lista.innerHTML = "";
 
     carrinho.forEach(function(item){
-        lista.innerHTML += "<p>✓ " + item + "</p>";
+
+        lista.innerHTML += "<p>✓ " + item.nome + "</p>";
+
+        total += item.preco;
+
     });
 
     document.getElementById("total").innerHTML =
-        "Itens no carrinho: " + carrinho.length;
+        "Total: R$ " + total.toFixed(2).replace(".", ",");
 }
