f47d6ffa9a607b81a64592154c39be5ac609d3c6
CantinhodasMassas <simiaomartinsgisleide@gmail.com>
2026-07-17 16:20:34 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ const valorTotal = document.getElementById("valorTotal");
 
 // Atualiza o valor da marmita
 
-function atualizarValor(){
+function atualizarValor() {
 
-    let total = 17.90;
+    let total = 0;
 
-    if(tamanho.value === "750 ml"){
+    if (tamanho.value === "500") {
+        total = 17.90;
+    }
+
+    if (tamanho.value === "750") {
         total = 24.90;
     }
 
-    if(proteinaExtra.value !== "Nenhuma"){
+    if (
+        tamanho.value !== "" &&
+        proteinaExtra.value !== "Nenhuma"
+    ) {
         total += 3;
     }
 
@@ -26,7 +33,6 @@ function atualizarValor(){
 // Eventos
 
 tamanho.addEventListener("change", atualizarValor);
-
 proteinaExtra.addEventListener("change", atualizarValor);
 
 // Inicia
