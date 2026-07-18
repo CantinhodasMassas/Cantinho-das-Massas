faa9ca50e0c85a680f2b65d63a5cd572aa00b927
CantinhodasMassas <simiaomartinsgisleide@gmail.com>
2026-07-18 12:05:34 -0300

    Update script.js 2

--- script.js
+++ script.js
@@ -88,6 +88,25 @@ function atualizarCarrinho() {
 
     }
 
+    let total = 0;
+
+    carrinho.forEach((item) => {
+
+        total += item.preco;
+
+        listaCarrinho.innerHTML += `
+            <div class="carrinho-item">
+                <span>${item.nome}</span>
+                <strong>R$ ${item.preco.toFixed(2).replace(".", ",")}</strong>
+            </div>
+        `;
+
+    });
+
+    totalCarrinho.innerHTML =
+        "R$ " + total.toFixed(2).replace(".", ",");
+
+}
   let total = 0;
 
 carrinho.forEach((item) => {
